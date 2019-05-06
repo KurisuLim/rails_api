04f6525d1006cf0be2df2270b62ca8cd5ccd8f3a
KurisuLim <kazuki1002003@yahoo.com>
2019-05-06 13:29:44 -0700

    added api call for sign-in-sign-out and log-out

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,24 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
-  before_Action :load_user, only: :create
+  before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
+
   #sign in
   def create
     if @user.valid_password?(sign_in_params[:password])
+      sign_in "user", @user
+      json_response "Signed In Successfully", true, {user: @user}, :ok
+    else
+      json_response "Unauthorized", false, {}, :unauthorized
+    end
+  end
+
+  # log out
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response "Log out Successfully", true, {}, :ok
   end
   
   private
@@ -17,6 +32,15 @@ class Api::V1::SessionsController < Devise::SessionsController
       return @user
     else
       json_response "Cannot get User", false, {}, :failure
+    end
+  end
+
+  def valid_token
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+    if @user
+      return @user
+    else
+      json_response "Invalid Token", false, {}, :failure 
     end
   end
 end
